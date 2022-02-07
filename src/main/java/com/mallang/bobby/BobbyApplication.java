ea227ad9c4ca2deaff9b1f818b53c3bd1c95a8c3
kimmallang <kimmallangcp@gmail.com>
2022-02-08 08:20:07 +0900

    프로젝트 생성

---
+++ src/main/java/com/mallang/bobby/BobbyApplication.java
@@ -0,0 +1,13 @@
+package com.mallang.bobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobbyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mallang/bobby/BobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mallang.bobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
