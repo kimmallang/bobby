244b3d8504b3fd30db6564bf626cabe6fd1e4090
kimmallang <kimmallangcp@gmail.com>
2022-02-08 09:12:32 +0900

    add : [공통] ResponseDto 구현

--- src/main/java/com/mallang/bobby/controller/HelloController.java
+++ src/main/java/com/mallang/bobby/controller/HelloController.java
@@ -3,10 +3,12 @@ package com.mallang.bobby.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mallang.bobby.domain.dto.ResponseDto;
+
 @RestController
 public class HelloController {
 	@GetMapping("/hello")
-	public String getHello() {
-		return "hello";
+	public ResponseDto getHello() {
+		return ResponseDto.builder().data("hello").build();
 	}
 }
---
+++ src/main/java/com/mallang/bobby/domain/dto/ResponseStatus.java
@@ -0,0 +1,15 @@
+package com.mallang.bobby.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ResponseStatus {
+	SUCCESS("SUCCESS", "요청이 처리되었습니다."),
+	ERROR("ERROR", "요청이 실패했습니다."),
+	BAD_REQUEST("BAD_REQUEST", "요청이 올바르지 않습니다.");
+
+	private String status;
+	private String message;
+}
