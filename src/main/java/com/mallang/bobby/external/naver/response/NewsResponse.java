8fb35005348cae601b3f75e8c8ff3638e1f0fef0
kimmallang <kimmallangcp@gmail.com>
2022-02-10 21:45:29 +0900

    update : [뉴스] 뉴스 API 연동

---
+++ src/main/java/com/mallang/bobby/external/naver/response/NewsResponse.java
@@ -0,0 +1,16 @@
+package com.mallang.bobby.external.naver.response;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class NewsResponse {
+	private String lastBuildDate;
+	private Integer total;
+	private Integer start;
+	private Integer display;
+	private List<NewsItemDto> items;
+}
