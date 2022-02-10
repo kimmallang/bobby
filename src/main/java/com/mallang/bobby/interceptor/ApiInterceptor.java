c8540b664537427df9d3997fcffd7eaba846956e
kimmallang <kimmallangcp@gmail.com>
2022-02-11 01:23:41 +0900

    update : [공통] referer 체크 기능 개선 및 코드 정리

--- src/main/java/com/mallang/bobby/interceptor/ApiInterceptor.java
+++ src/main/java/com/mallang/bobby/interceptor/ApiInterceptor.java
@@ -6,19 +6,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 public class ApiInterceptor implements HandlerInterceptor {
 	final String[] allowedDomain = new String[] {
-		"http://localhost",
+		"http://local.bobby.com:8080",
 		"https://mallang.herokuapp.com"
 	};
 
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws NoHandlerFoundException {
 		final String referer = request.getHeader("Referer");
 
+		if (!isAllowedDomain(request, referer)) {
+			throw new NoHandlerFoundException(request.getMethod(), request.getRequestURL().toString(), null);
+		}
+
+		return true;
+	}
+
+	private boolean isAllowedDomain(HttpServletRequest request, String referer) {
 		if (referer == null) {
-			return request.getRequestURI().startsWith("/swagger-ui/index.html");
+			return Arrays
+				.stream(allowedDomain)
+				.anyMatch(domain -> request
+					.getRequestURL()
+					.toString()
+					.startsWith(domain + "/swagger-ui/index.html"));
 		}
 
 		return Arrays.stream(allowedDomain).anyMatch(referer::startsWith);
