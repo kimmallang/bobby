ae7428ff2f41275932cb644f9da4b6016c6a7737
kimmallang <kimmallangcp@gmail.com>
2022-02-12 18:47:38 +0900

    add : [뉴스] 다중 저장 구현

--- src/main/java/com/mallang/bobby/domain/news/NewsService.java
+++ src/main/java/com/mallang/bobby/domain/news/NewsService.java
@@ -1,5 +1,8 @@
 package com.mallang.bobby.domain.news;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -14,18 +17,25 @@ public class NewsService {
 	private final NewsRepository newsRepository;
 
 	public NewsVo save(NewsVo newsVo) {
-		try {
-			final News news = modelMapper.map(newsVo, News.class);
-			final News savedNews = newsRepository.save(news);
-			return modelMapper.map(savedNews, NewsVo.class);
-		} catch (Exception e) {
-			log.error(String.valueOf(e));
-			throw e;
-		}
+		final News news = modelMapper.map(newsVo, News.class);
+		final News savedNews = newsRepository.save(news);
+
+		return modelMapper.map(savedNews, NewsVo.class);
+	}
+
+	public List<NewsVo> save(List<NewsVo> newsVoList) {
+		final List<News> newsList = newsVoList.stream()
+			.map(newsVo -> modelMapper.map(newsVo, News.class))
+			.collect(Collectors.toList());
+
+		return newsRepository.saveAll(newsList).stream()
+			.map(news -> modelMapper.map(news, NewsVo.class))
+			.collect(Collectors.toList());
 	}
 
 	public NewsVo find(Long id) {
 		final News news = newsRepository.findById(id).orElse(new News());
+
 		return modelMapper.map(news, NewsVo.class);
 	}
 }
--- src/test/java/com/mallang/bobby/domain/news/NewsServiceTest.java
+++ src/test/java/com/mallang/bobby/domain/news/NewsServiceTest.java
@@ -2,6 +2,10 @@ package com.mallang.bobby.domain.news;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -42,12 +46,30 @@ public class NewsServiceTest {
 		NewsVo savedNews = newsService.find(savedId);
 		news.setId(savedNews.getId());
 
-		assertEquals(news, savedNews, "news is not equals savedNews");
+		assertEquals(news, savedNews, "뉴스 단일 저장 실패");
+	}
+
+	@Test
+	void saveAll() {
+		final List<NewsVo> newsList = Arrays.asList("test1", "test2", "test3", "test4", "test5")
+			.stream()
+			.map(title -> NewsVo.builder()
+				.title("title")
+				.description("description")
+				.originalLink("originalLink")
+				.naverLink("naverLink")
+				.query("query")
+				.build())
+			.collect(Collectors.toList());
+
+		List<NewsVo> savedNewsList = newsService.save(newsList);
+
+		assertEquals(newsList.size(), savedNewsList.size(), "뉴스 다중 저장 실패");
 	}
 
 	@Test
 	void find() {
 		NewsVo news = newsService.find(8L);
-		assertEquals("test", news.getTitle(), "조회 실패");
+		assertEquals("test", news.getTitle(), "뉴스 단일 조회 실패");
 	}
 }
