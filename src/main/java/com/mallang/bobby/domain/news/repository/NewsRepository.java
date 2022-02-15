13d8fa97b38622544429afa6e76708393119a4a6
kimmallang <kimmallangcp@gmail.com>
2022-02-16 00:48:05 +0900

    add : [뉴스] 뉴스 조회 API 구현

--- src/main/java/com/mallang/bobby/domain/news/repository/NewsRepository.java
+++ src/main/java/com/mallang/bobby/domain/news/repository/NewsRepository.java
@@ -1,5 +1,7 @@
 package com.mallang.bobby.domain.news.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.mallang.bobby.domain.news.entity.News;
 @Repository
 public interface NewsRepository extends JpaRepository<News, Long> {
 	void deleteAllByQuery(String query);
+	Page<News> findAllByQuery(String query, Pageable pageable);
 }
--- src/main/java/com/mallang/bobby/domain/news/service/NewsService.java
+++ src/main/java/com/mallang/bobby/domain/news/service/NewsService.java
@@ -4,9 +4,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.mallang.bobby.domain.news.dto.NewsResponse;
 import com.mallang.bobby.domain.news.repository.NewsRepository;
 import com.mallang.bobby.domain.news.vo.NewsRequestQuery;
 import com.mallang.bobby.domain.news.vo.NewsVo;
@@ -22,6 +27,9 @@ public class NewsService {
 	private final NewsApiService newsApiService;
 	private final NewsRepository newsRepository;
 
+	private static final int pagingSize = 20;
+	private static final Sort sortByIdDesc = Sort.by(Sort.Direction.DESC, "id");
+
 	@Transactional
 	public List<NewsVo> refreshFromApi(NewsRequestQuery newsRequestQuery) {
 		final List<NewsVo> newsVoList = newsApiService.get(newsRequestQuery);
@@ -34,5 +42,18 @@ public class NewsService {
 		return newsRepository.saveAll(newsList).stream()
 			.map(news -> modelMapper.map(news, NewsVo.class))
 			.collect(Collectors.toList());
+	}
+
+	public NewsResponse get(NewsRequestQuery newsRequestQuery, int page) {
+		final Pageable pageable = PageRequest.of((page - 1), pagingSize, sortByIdDesc);
+		final Page<News> newsPage = newsRepository.findAllByQuery(newsRequestQuery.getQuery(), pageable);
+
+		return NewsResponse.builder()
+			.page(page)
+			.isLast(page >= newsPage.getTotalPages())
+			.items(newsPage.getContent().stream()
+				.map(news -> modelMapper.map(news, NewsVo.class))
+				.collect(Collectors.toList()))
+			.build();
 	}
 }
