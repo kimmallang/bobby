31844fd8a797b11facaeb538addbd243f304400c
kimmallang <kimmallangcp@gmail.com>
2022-02-18 01:17:25 +0900

    add : [공통] 리액트 서버에 대해 CORS 허용

--- src/main/java/com/mallang/bobby/config/WebConfig.java
+++ src/main/java/com/mallang/bobby/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.mallang.bobby.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,11 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new ApiInterceptor()).addPathPatterns("/**");
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:3000", "https://mallang.herokuapp.com");
 	}
 }
