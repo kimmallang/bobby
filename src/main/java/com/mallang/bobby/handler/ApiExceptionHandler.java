c73ed60384da28b674b5a63579198cb942dc49fd
kimmallang <kimmallangcp@gmail.com>
2022-02-17 20:50:53 +0900

    update : [공통] referer 체크 개선 > 커스텀 Exception 적용

--- src/main/java/com/mallang/bobby/handler/ApiExceptionHandler.java
+++ src/main/java/com/mallang/bobby/handler/ApiExceptionHandler.java
@@ -6,12 +6,20 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.mallang.bobby.dto.ResponseDto;
 import com.mallang.bobby.dto.ResponseStatus;
+import com.mallang.bobby.exception.NotAllowedDomainException;
 
 @RestControllerAdvice
 class ApiExceptionHandler {
 	@ExceptionHandler(NoHandlerFoundException.class)
 	public ResponseDto handleNotFound() {
 		return ResponseDto.builder().status(ResponseStatus.BAD_REQUEST).build();
+	}
+
+	@ExceptionHandler(NotAllowedDomainException.class)
+	public ResponseDto handleForbidden(NotAllowedDomainException e) {
+		return ResponseDto.builder().status(ResponseStatus.FORBIDDEN)
+			.data(e.getMessage())
+			.build();
 	}
 
 	@ExceptionHandler(Exception.class)
